131b01e73ca87c419b2846504eff93d9c223c0f0
jakubiakdev <jakubiakdev@gmail.com>
2023-08-10 15:04:09 +0200

    Allow for non-strings in JsonReplacerBuilder

--- src/main/java/de/cronn/assertions/validationfile/replacements/JsonReplacerBuilder.java
+++ src/main/java/de/cronn/assertions/validationfile/replacements/JsonReplacerBuilder.java
@@ -7,7 +7,11 @@ import de.cronn.assertions.validationfile.normalization.ValidationNormalizer;
 
 public class JsonReplacerBuilder extends AbstractJsonReplacer<JsonReplacerBuilder> {
 
-	private String valueToReplace = ".+?";
+	private String valueToReplace =
+			"(null|true|false|" + // booleans
+			"-?\\d+(\\.\\d+)?|" + // numbers
+			"(\".+?(?<!\\\\)\"))"; // strings; don't stop matching if it's escaped with \
+
 	private String replacementValue = "[masked]";
 
 	@Override
@@ -16,13 +20,27 @@ public class JsonReplacerBuilder extends AbstractJsonReplacer<JsonReplacerBuilde
 		Objects.requireNonNull(replacementValue);
 
 		String keyWithQuotationMarks = "\"" + key + "\"";
-		Pattern pattern = Pattern.compile(keyWithQuotationMarks + COLON_WITH_WHITESPACES_GROUP + "\"" + valueToReplace + "\"");
+		Pattern pattern = Pattern.compile(keyWithQuotationMarks + COLON_WITH_WHITESPACES_GROUP + valueToReplace);
 		String replacement = keyWithQuotationMarks + "$1" + "\"" + replacementValue + "\"";
 
 		return new Replacer(pattern, replacement);
 	}
 
+	/**
+	 * @deprecated use withStringValue instead
+	 */
+	@Deprecated()
 	public JsonReplacerBuilder withValue(String value) {
+		this.valueToReplace = "\"" + value + "\"";
+		return this;
+	}
+
+	public JsonReplacerBuilder withStringValue(String value) {
+		this.valueToReplace = "\"" + value + "\"";
+		return this;
+	}
+
+	public JsonReplacerBuilder withRawValue(String value) {
 		this.valueToReplace = value;
 		return this;
 	}
